9fa08cff8d28ca47d595d8f25512e804e98314ee
Heather Gallop <doyouseeitmyway@gmail.com>
2018-05-18 12:48:49 -0400

    Add menu folder to res. Create menu file. Add action buttons. Update manifest.

--- app/src/main/java/com/example/android/simpletoolbarpractice/MainActivity.java
+++ app/src/main/java/com/example/android/simpletoolbarpractice/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.simpletoolbarpractice;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSupportActionBar(findViewById(R.id.my_toolbar));
+        setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                Toast.makeText(this, "You clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                Toast.makeText(this, "You clicked", Toast.LENGTH_SHORT).show();
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
